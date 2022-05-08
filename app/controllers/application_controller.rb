592ff6f2e2d7faa9573560b13f70e8d376a8648f
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-05-07 17:37:41 -0700

    Enables authorize_request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::API
   before_action :authorize_request
 
   def authorize_request
-    #   header_authorization = request.headers['Authorization']
-    #   token = header_authorization.split(' ').last if header_authorization
-    #   begin
-    #     decode_jwt(token)
-    #   rescue StandardError
-    #     render json: { error: 'unauthorized' }, status: :unauthorized
-    #   end
+    header_authorization = request.headers['Authorization']
+    token = header_authorization.split(' ').last if header_authorization
+    begin
+      decode_jwt(token)
+    rescue StandardError
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
   end
 end
